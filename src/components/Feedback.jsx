aa31f3abcf72caa12c645349897736089ce225c8
VikaVelhan <velganvika@gmail.com>
2023-01-18 01:27:32 +0200

    Step1

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,21 +1,54 @@
 import React from 'react';
 
 class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countFeedbackGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+    console.log('клик по гуд');
+  };
+
+  countFeedbackNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+    console.log('клик по нейтрал');
+  };
+
+  countFeedbackBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+    console.log('клик по бед');
+  };
+
   render() {
     return (
       <div>
         <div>
           <h1>Please leave feedback</h1>
-          <button type="button">Good</button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button>
+          <button type="button" onClick={this.countFeedbackGood}>
+            Good
+          </button>
+          <button type="button" onClick={this.countFeedbackNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.countFeedbackBad}>
+            Bad
+          </button>
         </div>
         <div>
           <h2>Statistics</h2>
           <ul>
-            <li>Good:</li>
-            <li>Neutral:</li>
-            <li>Bad:</li>
+            <li>Good:{this.state.good}</li>
+            <li>Neutral:{this.state.neutral}</li>
+            <li>Bad:{this.state.bad}</li>
             <li>Total:</li>
             <li>Positive feedbac:</li>
           </ul>
